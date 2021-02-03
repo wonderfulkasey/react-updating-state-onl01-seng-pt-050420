249c4d8a06741021d966f9bcae52724aa75de64b
Kasey Arnold <kaseyarnold15@gmail.com>
2021-02-03 19:05:24 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ClickityClick from './components/ClickityClick';
 
 ReactDOM.render(
   <div>
-    Mount Components Here
+    Mount Components Here<ClickityClick />
   </div>,
   document.getElementById('root')
 );
